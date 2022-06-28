8453720d6b6b379abec1d4d3a65bccd982c6d33d
Kajetan Maślak <kajtek.maslak@icloud.com>
2022-06-28 19:18:07 +0200

    Created new Blazor WebAssembly application with ASP.NET Core API

---
+++ FinanceApp/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Duende.IdentityServer.EntityFramework.Options;
+using FinanceApp.Server.Models;
+
+namespace FinanceApp.Server.Data;
+
+public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(
+        DbContextOptions options,
+        IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+    {
+    }
+}
---
+++ FinanceApp/Server/Models/ApplicationUser.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FinanceApp.Server.Models;
+
+public class ApplicationUser : IdentityUser
+{
+}

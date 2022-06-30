645c16d4e87a0e96b479d824142e04d5d95da53e
Kajetan Maślak <kajtek.maslak@icloud.com>
2022-06-30 19:00:09 +0200

    Created StockChart component with implemented logic. [Exception Collection was modified?]

---
+++ FinanceApp/Shared/DTO/StockFinanceDTO.cs
@@ -0,0 +1,11 @@
+namespace FinanceApp.Shared.DTO;
+
+public class StockFinanceDTO
+{
+    public float C { get; set; }
+    public float O { get; set; }
+    public float V { get; set; }
+    public float H { get; set; }
+    public float L { get; set; }
+    public DateTime T { get; set; }
+}
